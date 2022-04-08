84eab20498cb7150f3b609896b4cd0f18c7e8b5a
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2022-04-09 01:22:20 +0200

    Make application a Protoc plugin

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
+use std::error::Error;
+use std::io;
+
+use protobuf::plugin::{CodeGeneratorRequest, CodeGeneratorResponse, CodeGeneratorResponse_File};
+use protobuf::Message;
+
 use crate::pretty::Prettify;
 
 mod ast;
 mod pretty;
 
-fn main() {
-    let import1 = ast::ImportDeclaration {
-        source: ast::Literal::String("./foo.js".to_string()),
-        specifiers: vec![
-            ast::ImportSpecifier::Named(ast::ImportNamedSpecifier {
-                local: ast::Identifier("test".to_string()),
-                imported: ast::Identifier("test".to_string()),
-            }),
-            ast::ImportSpecifier::Default(ast::ImportDefaultSpecifier {
-                local: ast::Identifier("foo".to_string()),
-            }),
-        ],
-    };
-
-    let import2 = ast::ImportDeclaration {
-        source: ast::Literal::String("./foo.js".to_string()),
-        specifiers: vec![ast::ImportSpecifier::Namespace(
-            ast::ImportNamespaceSpecifier {
-                local: ast::Identifier("FOO".to_string()),
-            },
-        )],
-    };
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stdin = io::stdin();
+    let request = CodeGeneratorRequest::parse_from_reader(&mut stdin)?;
 
     let module = ast::Module {
-        declarations: vec![
-            ast::Declaration::Import(import1),
-            ast::Declaration::Import(import2),
-        ],
+        declarations: request
+            .proto_file
+            .iter()
+            .flat_map(|proto| proto.message_type.iter())
+            .map(|descr| descr.get_name())
+            .map(|name| ast::ImportDeclaration {
+                source: ast::Literal::String(format!("./{}.js", name)),
+                specifiers: vec![ast::ImportSpecifier::Named(ast::ImportNamedSpecifier {
+                    local: id(name),
+                    imported: id(name),
+                })],
+            })
+            .map(ast::Declaration::Import)
+            .collect(),
     };
 
-    println!("{}", module.to_pretty(20));
-    println!("{}", module.to_pretty(100));
+    let mut file = CodeGeneratorResponse_File::new();
+    file.set_name("index.ts".to_string());
+    file.set_content(module.to_pretty(80));
+
+    let mut response = CodeGeneratorResponse::new();
+    response.file.push(file);
+    response.write_to_writer(&mut io::stdout())?;
+    Ok(())
+}
+
+fn id<I: Into<String>>(par: I) -> ast::Identifier {
+    ast::Identifier(par.into())
 }
