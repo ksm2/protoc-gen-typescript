f5533495397e9b6222d4325fe30e97fc626807d2
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2022-04-09 20:09:57 +0200

    Refactor towards SWC

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::error::Error;
 use std::io;
+use std::sync::Arc;
 
 use protobuf::plugin::{CodeGeneratorRequest, CodeGeneratorResponse, CodeGeneratorResponse_File};
 use protobuf::Message;
-
-use crate::pretty::Prettify;
-
-mod ast;
-mod pretty;
+use swc::common::collections::AHashMap;
+use swc::common::SourceMap;
+use swc::common::Span;
+use swc::config::SourceMapsConfig;
+use swc::ecmascript::ast;
+use swc::ecmascript::ast::EsVersion;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let cm = Arc::new(SourceMap::default());
+    let c = swc::Compiler::new(cm);
+
     let mut stdin = io::stdin();
     let request = CodeGeneratorRequest::parse_from_reader(&mut stdin)?;
 
@@ -19,41 +24,71 @@ fn main() -> Result<(), Box<dyn Error>> {
         .flat_map(|proto| proto.message_type.iter())
         .collect::<Vec<_>>();
 
-    let module = ast::Module::new(
-        messages
+    let module = ast::Module {
+        span: Span::default(),
+        shebang: None,
+        body: messages
             .iter()
             .map(|descr| descr.get_name())
-            .map(|name| {
-                ast::ExportDeclaration::From(ast::FromClause {
-                    source: ast::Literal::String(format!("./{}.js", name)),
-                    export: ast::ExportFromClause::Named(ast::NamedExports {
-                        exports: vec![ast::ExportNamedSpecifier::new(id(name))],
-                    }),
-                })
+            .map(|name| ast::NamedExport {
+                span: Span::default(),
+                specifiers: vec![ast::ExportSpecifier::Named(ast::ExportNamedSpecifier {
+                    span: Span::default(),
+                    exported: None,
+                    orig: ast::ModuleExportName::Ident(id(name)),
+                    is_type_only: false,
+                })],
+                type_only: false,
+                src: Some(ast::Str::from(format!("./{}.js", name))),
+                asserts: None,
             })
-            .map(ast::Declaration::Export)
+            .map(ast::ModuleDecl::ExportNamed)
+            .map(ast::ModuleItem::ModuleDecl)
             .collect(),
-    );
+    };
 
     let mut response = CodeGeneratorResponse::new();
-    response.file.push(file("index.ts", module));
+    response.file.push(file(&c, "index.ts", &module));
     for message in messages {
-        let module = ast::Module::new(vec![]);
+        let module = ast::Module {
+            span: Span::default(),
+            shebang: None,
+            body: vec![],
+        };
         response
             .file
-            .push(file(format!("{}.ts", message.get_name()), module))
+            .push(file(&c, format!("{}.ts", message.get_name()), &module))
     }
     response.write_to_writer(&mut io::stdout())?;
     Ok(())
 }
 
-fn file(filename: impl Into<String>, module: ast::Module) -> CodeGeneratorResponse_File {
+fn file(
+    c: &swc::Compiler,
+    filename: impl Into<String>,
+    module: &ast::Module,
+) -> CodeGeneratorResponse_File {
+    let ast_printed = c
+        .print(
+            module,
+            None,
+            None,
+            false,
+            EsVersion::Es2022,
+            SourceMapsConfig::Bool(false),
+            &AHashMap::default(),
+            None,
+            false,
+            None,
+        )
+        .expect("Failed to print");
+
     let mut file = CodeGeneratorResponse_File::new();
     file.set_name(filename.into());
-    file.set_content(module.to_pretty(80));
+    file.set_content(ast_printed.code);
     file
 }
 
-fn id(par: impl Into<String>) -> ast::Identifier {
-    ast::Identifier(par.into())
+fn id(str: &'_ str) -> ast::Ident {
+    ast::Ident::new(str.into(), Span::default())
 }
